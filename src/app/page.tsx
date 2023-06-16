7ff2d75171e2a5e8737c040561cea1326e43ccd4
Brian Nguyen <nguyenbrian604@gmail.com>
2023-06-15 20:56:07 -0700

    Update app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,7 @@ export default function Home() {
   return (
     <>
       <div
-        className="EntireBody h-full flex-col justify-center items-center"
-        style={{ backgroundColor: "#082032" }}
+        className="EntireBody "
       >
         <div className="NavBar">
           <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
