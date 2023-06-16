5d8ecc1a193ee0fff9ceeae322c3fe19db0deb7a
Brian Nguyen <nguyenbrian604@gmail.com>
2023-06-15 19:43:32 -0700

    Create page.tsx

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from "next/link"
+
+export default function Login() {
+    return (
+      <>
+            <div>page</div>
+            <Link href="/">Link To Home Page</Link>
+      </>
+    );
+}
