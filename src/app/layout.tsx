7b960a0528c7733cc23fc3519077b3edc7bca517
Brian Nguyen <nguyenbrian604@gmail.com>
2023-06-15 20:56:26 -0700

    Update app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className="h-full flex-col justify-center items-center"
+        style={{ backgroundColor: "#082032" }}
+      >
+        {children}
+      </body>
     </html>
   );
 }
