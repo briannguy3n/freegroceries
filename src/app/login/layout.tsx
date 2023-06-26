6df4a658a1a4cb9c801b0ab66088487679093451
Brian Nguyen <nguyenbrian604@gmail.com>
2023-06-26 10:50:51 -0700

    yep

--- src/app/login/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function LoginLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-    return <div className="text-white ">{children}</div>;
-}
