619630149ef724c58f2a51ed3911682da57d210f
Brian Nguyen <nguyenbrian604@gmail.com>
2023-06-27 23:28:05 -0700

    Update

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -39,7 +39,7 @@ const RegisterModal = () => {
         registerModal.onClose()
       })
       .catch((error) => {
-        toast.error(error)
+        toast.error("Something went wrong.")
       })
       .finally(() => {
         setIsLoading(false)
@@ -77,6 +77,47 @@ const RegisterModal = () => {
     </div>
   )
 
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div
+        className="
+          text-neutral-500 
+          text-center 
+          mt-4 
+          font-light
+        "
+      >
+        <p>
+          Already have an account?
+          <span
+            onClick={registerModal.onClose}
+            className="
+          text-neutral-800
+          cursor-pointer 
+          hover:underline
+        "
+          >
+            {" "}
+            Log in
+          </span>
+        </p>
+      </div>
+    </div>
+  )
+
   return (
     <Modal
       disabled={isLoading}
@@ -86,6 +127,7 @@ const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   )
 }
