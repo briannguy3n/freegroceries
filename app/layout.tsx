1fd37474179c625a2b8ab3120767920e0745b9a2
Brian Nguyen <nguyenbrian604@gmail.com>
2023-07-15 15:42:19 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ import RegisterModal from "./components/modals/RegisterModal"
 import MenuItem from "./components/navbar/MenuItem"
 import UserMenu from "./components/navbar/UserMenu"
 import ToasterProvider from "./providers/ToasterProvider"
+import Test from "./components/Test"
+import Test2 from "./components/Test2"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -28,10 +30,12 @@ export default function RootLayout({
         <ClientOnly>
           <ToasterProvider />
           <UserMenu />
-          <RegisterModal  />
+          <Test />
+          <Test />
+          <Test2 />
+          <RegisterModal />
           <HomePage />
           <Sidebar />
-
         </ClientOnly>
       </body>
     </html>
