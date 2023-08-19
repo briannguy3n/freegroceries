c34436f98bac776ccb447e7651d2cbe0f367ac9b
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-19 17:30:50 -0400

    Update page.tsx
    Update page.tsx
    
    Update page.tsx
    
    Update page.tsx
    
    Update page.tsx
    
    Update page.tsx
    
    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
-import Image from 'next/image'
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar/Navbar";
 
 export default function Home() {
-  return <div className="h-screen w-screen bg-green-700	"></div>;
+  return (
+    <div className="h-screen w-screen bg-green-900 justify-center items-center">
+      {/* <div className="bg-green-700"></div> */}
+      <Navbar />
+      <div className="flex w-screen h-screen justify-center items-center">
+        {/* <Register /> */}
+        <Login />
+      </div>
+    </div>
+  );
 }
