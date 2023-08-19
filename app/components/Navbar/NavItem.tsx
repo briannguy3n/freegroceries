e74d62aedb0b51a3ba1bbbe703b3eaa08452d739
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-19 18:38:52 -0400

    Update NavItem.tsx
    Update NavItem.tsx
    
    Update NavItem.tsx
    
    Update NavItem.tsx
    
    Update NavItem.tsx

--- app/components/Navbar/NavItem.tsx
+++ app/components/Navbar/NavItem.tsx
@@ -1,5 +1,12 @@
-const NavItem = () => {
-    return ( <div></div> );
-}
- 
+import React from "react";
+
+const NavItem = (props) => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-1">
+      <div className={`text-xl text-white`}>{props.icon}</div>
+      <p className="text-white uppercase font-semibold text-xs">{props.title}</p>
+    </div>
+  );
+};
+
 export default NavItem;
