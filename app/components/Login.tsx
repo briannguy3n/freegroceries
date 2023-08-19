76ca6d24e7230ea754e3651e600922ad558767f5
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-19 18:01:48 -0400

    Update Login.tsx

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -12,14 +12,13 @@ const Login = () => {
   return (
     <Card color="transparent" shadow={false}>
       <Typography variant="h4" color="white">
-        Sign Up
+        Login
       </Typography>
       <Typography color="white" className="mt-1 font-normal">
-        Enter your details to register.
+        Enter your details to login.
       </Typography>
       <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
         <div className="mb-4 flex flex-col gap-6">
-          <Input size="lg" label="Name" color="white" crossOrigin={undefined} />
           <Input
             size="lg"
             label="Email"
@@ -34,34 +33,10 @@ const Login = () => {
             crossOrigin={undefined}
           />
         </div>
-        <Checkbox
-          label={
-            <Typography
-              variant="small"
-              color="white"
-              className="flex items-center font-normal"
-            >
-              I agree the
-              <a
-                href="#"
-                className="font-medium transition-colors hover:text-white-900"
-              >
-                &nbsp;Terms and Conditions
-              </a>
-            </Typography>
-          }
-          containerProps={{ className: "-ml-2.5" }}
-          crossOrigin={undefined}
-        />
+
         <Button className="mt-6" fullWidth>
-          Register
+          Login
         </Button>
-        <Typography color="white" className="mt-4 text-center font-normal">
-          Already have an account?{" "}
-          <a href="#" className="font-medium text-white-900">
-            Sign In
-          </a>
-        </Typography>
       </form>
     </Card>
   );
