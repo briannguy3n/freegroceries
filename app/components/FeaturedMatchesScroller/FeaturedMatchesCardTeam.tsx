c589152955c9a1959d341490ed9dc23f8438b20b
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-22 21:56:51 -0700

    Update FeaturedMatchesCardTeam.tsx

--- app/components/FeaturedMatchesScroller/FeaturedMatchesCardTeam.tsx
+++ app/components/FeaturedMatchesScroller/FeaturedMatchesCardTeam.tsx
@@ -3,21 +3,30 @@
 import React, { useState, useEffect } from "react"
 
 const FeaturedMatchesCardTeam = () => {
-  const [data, setData] = useState([])
+  const [matchData, setMatchData] = useState(null)
 
   useEffect(() => {
-    // Fetch data from the API here
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    const apiKey = "YOUR_API_KEY"
+    const apiUrl = "https://api.example.com/data"
+
+    fetch(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    })
       .then((response) => response.json())
-      .then((data) => setData(data))
+      .then((data) => setMatchData(data))
       .catch((error) => console.error("Error fetching data:", error))
   }, [])
 
+  if (!matchData) {
+    return <div>Loading...</div>
+  }
+  
   return (
     <div className="flex flex-col items-center justify-center text-white">
       <p>Team Logo Image</p>
       <p>Team Name</p>
-      
     </div>
   )
 }
