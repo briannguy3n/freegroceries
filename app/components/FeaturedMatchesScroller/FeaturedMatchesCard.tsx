95eb44fd53daa73e7cc26882d8ff34d90592f99d
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-22 20:55:04 -0700

    Update FeaturedMatchesCard.tsx
    Update FeaturedMatchesCard.tsx

--- app/components/FeaturedMatchesScroller/FeaturedMatchesCard.tsx
+++ app/components/FeaturedMatchesScroller/FeaturedMatchesCard.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React from "react"
+import FeaturedMatchesCardTeam from "./FeaturedMatchesCardTeam"
 
 const FeaturedMatchesCard = () => {
-    return ( <div></div> );
+  return (
+    <div
+      className="
+        max-w-sm 
+        p-6
+        bg-freegroceries-900
+        rounded-lg
+        shadow
+        flex
+        flex-col
+        justify-center
+        items-center
+        gap-2
+        "
+    >
+      <p className="text-white">Wed 16:05</p>
+      <div
+        className="
+        flex
+        flex-row
+        gap-2
+        items-center
+        justify-center
+        w-full
+        mx-auto"
+      >
+        <FeaturedMatchesCardTeam />
+        <p className="text-white">@</p>
+        <p className="text-white">Away Team</p>
+      </div>
+      <div className="flex flex-row gap-2">
+        <p className="text-white">Home Team Odds</p>
+        <p className="text-white">Away Team Odds</p>
+      </div>
+    </div>
+  )
 }
- 
-export default FeaturedMatchesCard;
+
+export default FeaturedMatchesCard
