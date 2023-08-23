8976af4eb330168cb0816611447ed4f09c7c914e
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-22 21:04:09 -0700

    Update FeaturedMatchesCardTeam.tsx
    Update FeaturedMatchesCardTeam.tsx

--- app/components/FeaturedMatchesScroller/FeaturedMatchesCardTeam.tsx
+++ app/components/FeaturedMatchesScroller/FeaturedMatchesCardTeam.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+"use client"
+
+import React, { useState, useEffect } from "react"
 
 const FeaturedMatchesCardTeam = () => {
-    return ( <div></div> );
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    // Fetch data from the API here
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error fetching data:", error))
+  }, [])
+
+  return (
+    <div className="flex flex-col items-center justify-center text-white">
+      <p>Team Logo Image</p>
+      <p>Team Name</p>
+      
+    </div>
+  )
 }
- 
-export default FeaturedMatchesCardTeam;
+
+export default FeaturedMatchesCardTeam
