9224d7b81ac856e5190bf194f2033a94b5004f78
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-22 20:17:04 -0700

    Update page.tsx
    Update page.tsx
    
    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar/Navbar";
+import FeaturedMatchesScroller from "./components/FeaturedMatchesScroller/FeaturedMatchesScroller";
+import React from "react";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-green-900 justify-center items-center">
+    <div className="h-screen w-screen bg-freegroceries-200 justify-center items-center">
       {/* <div className="bg-green-700"></div> */}
       <Navbar />
       <div className="flex w-screen h-screen justify-center items-center">
+        <FeaturedMatchesScroller />
         {/* <Register /> */}
-        <Login />
+        {/* <Login /> */}
       </div>
     </div>
   );
