38b38f2e986f204862fde90ea5119211ee661bcb
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-25 14:13:29 -0700

    Update FeaturedMatchesCard.tsx

--- app/components/FeaturedMatchesScroller/FeaturedMatchesCard.tsx
+++ app/components/FeaturedMatchesScroller/FeaturedMatchesCard.tsx
@@ -1,40 +1,14 @@
 import React from "react"
-import FeaturedMatchesCardTeam from "./FeaturedMatchesCardTeam"
-import sportsdata from "../../sportsdata"
+import FeaturedMatchesMatchUp from "./FeaturedMatchesMatchUp"
+import sportsdata from "@/app/sportsdata"
+import FeaturedMatchesOddsSection from "./FeaturedMatchesOddsSection/FeaturedMatchesOddsSection"
 
 const FeaturedMatchesCard = () => {
   const firstMatch = sportsdata.gamedata[0]
-
-  if (!firstMatch) {
-    return null
-  }
-
+  
   //   --------------------------------
-
-  // Calculating Betting Odds
-
-  const homeTeamDecimalOdds =
-    firstMatch.bookmakers[0].markets[0].outcomes[0].price
-  const awayTeamDecimalOdds =
-    firstMatch.bookmakers[0].markets[0].outcomes[1].price
-
-  const calculateAmericanOdds = (decimalOdds: number) => {
-    if (decimalOdds >= 2.0) {
-      const americanOdds = Math.round((decimalOdds - 1) * 100)
-      return americanOdds >= 0 ? `+${americanOdds}` : americanOdds
-    } else if (decimalOdds > 1.0 && decimalOdds < 2.0) {
-      const americanOdds = Math.round(-100 / (decimalOdds - 1))
-      return americanOdds >= 0 ? `+${americanOdds}` : americanOdds
-    }
-    return "+0" // Handle any other cases
-  }
-
-  const homeTeamAmericanOdds = calculateAmericanOdds(homeTeamDecimalOdds)
-  const awayTeamAmericanOdds = calculateAmericanOdds(awayTeamDecimalOdds)
-
+  
   const changeDateTime = () => {}
-
-  //   --------------------------------
 
   return (
     <div
@@ -55,38 +29,8 @@ const FeaturedMatchesCard = () => {
         <p className="text-white">Wed 16:05</p>
       </div>
 
-      <div
-        className="
-        flex
-        flex-row
-        items-center
-        justify-center
-        w-full
-        mt-4
-      "
-      >
-        <FeaturedMatchesCardTeam
-          teamname={firstMatch.home_team}
-          odds={
-            <>
-              <span className="text-white pr-2">1</span>
-              <span className="font-light">{homeTeamAmericanOdds}</span>
-            </>
-          }
-        />
-
-        <p className="text-white bottom-14 relative">@</p>
-
-        <FeaturedMatchesCardTeam
-          teamname={firstMatch.away_team}
-          odds={
-            <>
-              <span className="text-white pr-2">2</span>
-              <span className="font-light">{awayTeamAmericanOdds}</span>
-            </>
-          }
-        />
-      </div>
+      <FeaturedMatchesMatchUp />
+      <FeaturedMatchesOddsSection />
     </div>
   )
 }
