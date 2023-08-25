29dba3f205d338a294e1bde6898084cfed877963
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-25 14:42:05 -0700

    Update MatchContext.js

--- app/MatchContext.js
+++ app/MatchContext.js
@@ -1,7 +1,7 @@
-"use client"
-
 import React, { createContext, useContext } from "react";
 import sportsdata from "./sportsdata";
+
+//---------------------------
 
 const MatchContext = createContext();
 
