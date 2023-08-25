9ea77df378fd9df560a6797e4b94daf02ba85b7b
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-25 14:31:47 -0700

    Update FeaturedMatchesCard.tsx
    Update FeaturedMatchesCard.tsx

--- app/components/FeaturedMatchesScroller/FeaturedMatchesCard.tsx
+++ app/components/FeaturedMatchesScroller/FeaturedMatchesCard.tsx
@@ -1,30 +1,19 @@
-import React from "react"
+import React from "react";
 import FeaturedMatchesMatchUp from "./FeaturedMatchesMatchUp"
-import sportsdata from "@/app/sportsdata"
 import FeaturedMatchesOddsSection from "./FeaturedMatchesOddsSection/FeaturedMatchesOddsSection"
+import { useMatchContext } from "@/app/MatchContext";
 
 const FeaturedMatchesCard = () => {
-  const firstMatch = sportsdata.gamedata[0]
-  
+  const { firstMatch } = useMatchContext();
+
   //   --------------------------------
-  
+
   const changeDateTime = () => {}
 
   return (
-    <div
-      className="
-        bg-freegroceries-900
-        rounded-lg
-        shadow
-        flex
-        flex-col
-        justify-center
-        items-center
-        gap-4
-        w-96
-      "
-    >
-      <div className="flex flex-col items-center justify-center">
+    <div className="bg-freegroceries-900 rounded-lg shadow flex flex-col justify-center items-center gap-4 w-96">
+      
+      <div className="DateSection flex flex-col items-center justify-center">
         <p className="text-white mt-4">{firstMatch.commence_time}</p>
         <p className="text-white">Wed 16:05</p>
       </div>
