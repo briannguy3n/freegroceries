33b383a1d6138ff3a22599f021fb0d867b83df8f
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-25 14:31:46 -0700

    Update page.tsx
    Update page.tsx
    
    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,25 @@
-import Register from "./components/Register";
-import Login from "./components/Login";
-import Navbar from "./components/Navbar/Navbar";
-import FeaturedMatchesScroller from "./components/FeaturedMatchesScroller/FeaturedMatchesScroller";
-import React from "react";
+"use client"
+
+import Register from "./components/Register"
+import Login from "./components/Login"
+import Navbar from "./components/Navbar/Navbar"
+import FeaturedMatchesScroller from "./components/FeaturedMatchesScroller/FeaturedMatchesScroller"
+import React from "react"
+import { MatchProvider } from "./MatchContext"
 
 export default function Home() {
   return (
-    <div className="h-full w-screen bg-freegroceries-200 justify-center items-center">
+    <div className="h-full w-screen bg-freegroceries-100 justify-center items-center">
       {/* <div className="bg-green-700"></div> */}
       <Navbar />
       <div className="flex w-screen h-screen justify-center items-center">
-        <FeaturedMatchesScroller />
+        <MatchProvider>
+          <FeaturedMatchesScroller />
+        </MatchProvider>
         {/* <Register /> */}
         {/* <Login /> */}
       </div>
     </div>
-  );
+  )
 }
+
