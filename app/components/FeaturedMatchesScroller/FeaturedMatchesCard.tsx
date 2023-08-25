b0c14596615ec0eb30e504c6458c0a97876542e3
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-25 14:57:06 -0700

    Add DateTime Function

--- app/components/FeaturedMatchesScroller/FeaturedMatchesCard.tsx
+++ app/components/FeaturedMatchesScroller/FeaturedMatchesCard.tsx
@@ -6,16 +6,26 @@ import { useMatchContext } from "@/app/MatchContext";
 const FeaturedMatchesCard = () => {
   const { firstMatch } = useMatchContext();
 
-  //   --------------------------------
+// #region changeDateTime
 
-  const changeDateTime = () => {}
+const changeDateTime = (dateTime) => {
+  const options = { weekday: 'short', hour: '2-digit', minute: '2-digit', hour12: false };
+  const date = new Date(dateTime);
+
+  return date.toLocaleString('en-US', options);
+};
+
+const formattedDateTime = changeDateTime(firstMatch.commence_time);
+
+  // #endregion
+
+
 
   return (
     <div className="bg-freegroceries-900 rounded-lg shadow flex flex-col justify-center items-center gap-4 w-96">
       
       <div className="DateSection flex flex-col items-center justify-center">
-        <p className="text-white mt-4">{firstMatch.commence_time}</p>
-        <p className="text-white">Wed 16:05</p>
+        <p className="text-white mt-4">{formattedDateTime}</p>
       </div>
 
       <FeaturedMatchesMatchUp />
