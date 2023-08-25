402f11e17d30526191d5a90166e854c55af8d0a9
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-25 14:31:49 -0700

    Update FeaturedMatchesTeamOdds.tsx
    Update FeaturedMatchesTeamOdds.tsx

--- app/components/FeaturedMatchesScroller/FeaturedMatchesOddsSection/FeaturedMatchesTeamOdds.tsx
+++ app/components/FeaturedMatchesScroller/FeaturedMatchesOddsSection/FeaturedMatchesTeamOdds.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from "react"
 
-const FeaturedMatchesTeamOdds = (props) => {
+const FeaturedMatchesTeamOdds = ({ teamPlace, teamOdds }) => {
   return (
-    <div className="w-full h-14 hover:bg-freegroceries-200 items-center justify-center flex">
-      <span className="text-white pr-2">{props.teamPlace}</span>
-      <span className="font-light">{props.teamOdds}</span>
+    <div className="w-full h-20 hover:bg-freegroceries-200 items-center justify-center flex">
+      <span className="font-light text-white pr-2">{teamPlace}</span>
+      <span className="text-gold-200 ">{teamOdds}</span>
 
-    {/* 
+      {/* 
         <div className="hover:bg-freegroceries-200">
             <span className="text-white pr-2">2</span>
             <span className="font-light">awayTeamOdds</span>
         </div>  
-    */}     
-
+    */}
     </div>
   )
 }
