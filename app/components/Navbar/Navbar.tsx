9b5c341b80b780d1d9f577e8eea32b43c25f2341
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-25 18:28:20 -0700

    Update Navbar.tsx

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 const Navbar = () => {
-  return <nav className="bg-freegroceries-900 h-10 flex gap-10 w-screen">
+  return <nav className="bg-freegroceries-100 h-16 flex gap-10 w-screen drop-shadow">
     <h1 className="uppercase text-white font-bold text-3xl">Logo</h1>
     <h1 className="uppercase text-white font-bold text-3xl">Join</h1>
     <h1 className="uppercase text-white font-bold text-3xl">Login</h1>
