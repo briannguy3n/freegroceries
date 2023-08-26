5d660f777def6677e326449d50c12fb1b4a0d612
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-25 18:28:21 -0700

    Update BaseballBettingCard.tsx

--- app/components/BaseballBettingPage/BaseballBettingCard/BaseballBettingCard.tsx
+++ app/components/BaseballBettingPage/BaseballBettingCard/BaseballBettingCard.tsx
@@ -1,5 +1,17 @@
+import React from "react"
+import BaseballTeam from "./BaseballTeam"
+import BaseballMoneyline from "./BaseballMoneyline"
+
 const BaseballBettingCard = () => {
-    return ( <div></div> );
+  return (
+    <div className="bg-freegroceries-900 drop-shadow grid grid-cols-2 pt-4">
+      <div className="grid-rows-2 grid">
+        <BaseballTeam teamName="🦆HOU Astros" />
+        <BaseballTeam teamName="🦆DET Tigers" />
+      </div>
+      <BaseballMoneyline />
+    </div>
+  )
 }
- 
-export default BaseballBettingCard;
+
+export default BaseballBettingCard
