1e91504f6eec8eeeb5ee43de1bc3456370690228
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-25 18:28:23 -0700

    Update BaseballTeam.tsx

---
+++ app/components/BaseballBettingPage/BaseballBettingCard/BaseballTeam.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// @ts-ignore
+const BaseballTeam = (props) => {
+  return (
+    <div className="h-10">
+      <p className="ml-10">{props.teamName}</p>
+    </div>
+  )
+}
+
+export default BaseballTeam
