4bc67942225e19510bb5471d59025cf6a95fd939
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-25 18:28:22 -0700

    Update BaseballMoneyline.tsx

--- app/components/BaseballBettingPage/BaseballBettingCard/BaseballMoneyline.tsx
+++ app/components/BaseballBettingPage/BaseballBettingCard/BaseballMoneyline.tsx
@@ -1,5 +1,9 @@
 const BaseballMoneyline = () => {
-    return ( <div></div> );
+    return ( 
+    <div className="justify-self-center grid-rows-2 grid">
+        <p>+100</p>
+        <p>-200</p>
+    </div> );
 }
  
 export default BaseballMoneyline;
