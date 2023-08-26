b69576d5350e2bf939bbca76c208e361d5be8247
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-25 18:28:24 -0700

    Update BaseballBettingPageTopRow.tsx

--- app/components/BaseballBettingPage/BaseballBettingPageTopRow.tsx
+++ app/components/BaseballBettingPage/BaseballBettingPageTopRow.tsx
@@ -1,5 +1,10 @@
 const BaseballBettingPageTopRow = () => {
-    return ( <div></div> );
+  return (
+    <div className="w-screen grid-cols-2 grid bg-freegroceries-800 drop-shadow h-[30px]">
+      <p className="ml-10">Fri Aug 25</p>
+      <p className="justify-self-center">Money Line</p>
+    </div>
+  )
 }
- 
-export default BaseballBettingPageTopRow;
+
+export default BaseballBettingPageTopRow
