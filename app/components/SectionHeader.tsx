d0ff6c9f6625fe30176ea06720f8d3c295f9b909
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-25 18:28:19 -0700

    Create SectionHeader.tsx

---
+++ app/components/SectionHeader.tsx
@@ -0,0 +1,17 @@
+const SectionHeader = () => {
+  return (
+    <div
+      className="
+        h-32
+      bg-freegroceries-200
+        w-full
+        drop-shadow 
+        font-bold
+        text-6xl
+        "
+    >Baseball
+    </div>
+  )
+}
+
+export default SectionHeader
