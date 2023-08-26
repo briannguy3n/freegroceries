a4d046991960939b98bdcfe88617f0f9901b371e
Brian Nguyen <nguyenbrian604@gmail.com>
2023-08-25 17:01:02 -0700

    Revert "Update Navbar.tsx"
    This reverts commit f9fcfe3948b61ae036e4d6c4ed8010cc5bc4f0db.

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,5 +1,28 @@
+import NavItem from "./NavItem";
+import {
+  GiAmericanFootballBall,
+  GiPokerHand,
+  GiHorseHead,
+} from "react-icons/gi";
+import { RiTimerFlashFill } from "react-icons/ri";
+import { IoLogoGameControllerA } from "react-icons/io";
+import { AiFillTrophy, AiFillGift } from "react-icons/ai";
+import React from "react";
+
 const Navbar = () => {
-  return ( <div></div> );
-}
- 
+  return (
+    <nav className="bg-freegroceries-900 p-4">
+      <div className="flex justify-evenly">
+        <NavItem title="Sports" icon={<GiAmericanFootballBall />} />
+        <NavItem title="Live Betting" icon={<RiTimerFlashFill />} />
+        <NavItem title="Casino" icon={<GiPokerHand />} />
+        <NavItem title="ESports" icon={<IoLogoGameControllerA />} />
+        <NavItem title="Racebook" icon={<GiHorseHead />} />
+        <NavItem title="Contests" icon={<AiFillTrophy />} />
+        <NavItem title="Promos" icon={<AiFillGift />} />
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
